0b3312b77ac7947e446eee9e74a1b54ece6cf8ff
Dean Rumsby <dean@deanrumsby.com>
2024-04-30 23:21:27 +0100

    feat: implement load and draw function

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
 
-const PROGRAM_START: u16 = 0x200;
+const PROG_START: u16 = 0x200;
 const OPCODE_SIZE: u16 = 2;
 const V_REG_COUNT: usize = 16;
 const STACK_SIZE: usize = 16;
@@ -9,7 +9,7 @@ const RAM_SIZE: usize = 4096;
 const FRAME_SIZE: usize = 64 * 32 * 4;
 
 pub const Cpu = struct {
-    pc: u16 = PROGRAM_START,
+    pc: u16 = PROG_START,
     i: u16 = 0x0000,
     sp: u8 = 0x00,
     st: u8 = 0x00,
@@ -21,6 +21,10 @@ pub const Cpu = struct {
 
     pub fn init() Cpu {
         return .{};
+    }
+
+    pub fn load(self: *Cpu, bytes: []const u8) void {
+        @memcpy(self.ram[PROG_START .. PROG_START + bytes.len], bytes);
     }
 
     pub fn step(self: *Cpu) !void {
@@ -57,6 +61,7 @@ pub const Cpu = struct {
             },
             0xa => .{ .LDI = nnn },
             0xb => .{ .JMP = nnn + self.v[0] },
+            0xd => .{ .DRW = .{ self.v[x], self.v[y], @intCast(n) } },
             0xf => switch (nn) {
                 0x07 => .{ .LDR = .{ &self.v[x], self.dt } },
                 0x15 => .{ .LDR = .{ &self.dt, self.v[x] } },
@@ -71,30 +76,48 @@ pub const Cpu = struct {
         switch (instruction) {
             .ADD => |p| p[0].* +%= p[1],
             .CLS => self.frame = [_]u8{0} ** FRAME_SIZE,
-            // .DRW => |d| self.draw(d[0], d[1], d[2]),
+            .DRW => |d| self.draw(d[0], d[1], d[2]),
             .JMP => |addr| self.pc = addr,
             .LDI => |word| self.i = word,
             .LDR => |p| p[0].* = p[1],
         }
     }
 
-    // fn draw(self: *Cpu, x: u8, y: u8, h: u4) void {
-    //     const bytes = self.ram[self.i .. self.i + h];
-    //     for (bytes, 0..h) |byte, i| {
-    //
-    //
-    //
-    // }
+    fn draw(self: *Cpu, x: u8, y: u8, h: u4) void {
+        const bytes = self.ram[self.i .. self.i + h];
+        for (bytes, 0..h) |byte, i| {
+            for (0..8) |j| {
+                const offset = (x + i) * 64 * 4 + (y + j) * 4;
+                const bit = (byte >> (7 - j)) & 1;
+                const pixel = &self.frame[offset + 3];
+                if (pixel.* == 1 and bit == 1) {
+                    self.v[0xf] = 1;
+                }
+                pixel.* = if (pixel.* != bit) 1 else 0;
+            }
+        }
+    }
 };
 
 const Instruction = union(enum) {
     ADD: struct { *u8, u8 },
     CLS,
-    // DRW: struct { u8, u8, u4 },
+    DRW: struct { u8, u8, u4 },
     JMP: u16,
     LDI: u16,
     LDR: struct { *u8, u8 },
 };
+
+test "load" {
+    var cpu = Cpu{};
+    const bytes = [_]u8{ 0x45, 0x23, 0x36 };
+    var end: usize = 2;
+    end += 1;
+    cpu.load(bytes[0..end]);
+    try testing.expect(cpu.ram[PROG_START] == 0x45);
+    try testing.expect(cpu.ram[PROG_START + 1] == 0x23);
+    try testing.expect(cpu.ram[PROG_START + 2] == 0x36);
+}
 
 test "fetch" {
     var cpu = Cpu{};

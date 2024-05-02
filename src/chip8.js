fea7d7144ca891ee8b19095fa2bf1554e42a605c
Dean Rumsby <dean@deanrumsby.com>
2024-05-02 20:52:59 +0100

    chore: remove some magic numbers

--- src/chip8.js
+++ src/chip8.js
@@ -1,15 +1,18 @@
 let wasm;
 
+const PROG_START = 0x200;
+const FRAME_SIZE = 64 * 32 * 4;
+
 WebAssembly.instantiateStreaming(fetch("./bin/chip8-vm.wasm")).then(
   (obj) => wasm = obj.instance.exports,
 );
 
 export function frame() {
-  return new Uint8ClampedArray(wasm.memory.buffer, wasm.frame_ptr(), 64 * 32 * 4);
+  return new Uint8ClampedArray(wasm.memory.buffer, wasm.frame_ptr(), FRAME_SIZE);
 }
 
 export function load(buffer) {
-  const offset = wasm.ram_ptr() + 0x200;
+  const offset = wasm.ram_ptr() + PROG_START;
   let dest = new Uint8Array(wasm.memory.buffer, offset, buffer.byteLength);
   dest.set(new Uint8Array(buffer));
 }
